12f06f70727ceb8cf68849f5f166425280928f5b
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2020-11-23 13:26:00 +0900

    defined convertToF and planned convert to C and planned displaying weather info

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ function formatDate(date) {
 }
 
 function displayCurrentTemperature(response) {
-  document.querySelector("#temperature").innerHTML = Math.round(
+  document.querySelector("#today-temp").innerHTML = Math.round(
     response.data.main.temp
   );
   document.querySelector("#search-city-input").innerHTML = response.data.name;
@@ -43,13 +43,49 @@ function searchCity(event) {
   h1.innerHTML = `${searchCityInput.value}`;
   let apiKey = "5888f249350e3151a80f06ae9f2e6ba9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayCurrentTemperature);
+axios.get(apiUrl).then((data) =>{
+console.log(data); 
+displayCurrentTemperature(data);
+}) 
+
 }
+
+function convertToF () {
+let temp = Number(document.querySelector("#today-temp").innerHTML);
+// console.log ("temp",temp);
+// console.log(typeof temp);
+const fahrenheitResult = (temp*9/5) + 32;
+// console.log("fahrenheit",fahrenheitResult);
+document.querySelector("#today-temp").innerHTML = fahrenheitResult;
+}
+
+//create a function called convert to C 
+//declaring a variable temp, selecting temperature ID with innerHTML replacing text and making sure it's a number not a string
+//declare const for the temperature conversion equation fahrenheit --> celsius = celsius result
+//selecting the text innerHTML with the ID of temperature and setting it equal to celsius result
+
 
 let searchCityForm = document.querySelector("#search-city-form");
 searchCityForm.addEventListener("submit", searchCity);
+
+let fahrenheitButton = document.querySelector("#fahrenheit-link");
+fahrenheitButton.addEventListener("click", convertToF);
+
+// celsius Button equal to document query Selector with ID of celsius link
+//add Event Listener to celsiusButton with function click convert to C 
 
 //Features
 let dateElement = document.querySelector("#todays-date-and-time");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
+
+//How to get the weather icons for openweathermap api
+//https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+//var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+//data.data.weather[0].icon
+//data.data.weather[0].description 
+
+//create a  function and name it display weather info
+//var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+//query select ID of weather icon and update the SRC to be weather icon url (refer to line 83 with the article)
+//query select ID of weather description and update innerHTML text to data.data.weather[0].description 
