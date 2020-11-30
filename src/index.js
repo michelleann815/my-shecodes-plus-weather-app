eda303697ba77902b7b2c7a34c88fc7058673c4a
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2020-11-30 21:29:01 +0900

    Fixed convert to F/C functions, added geolocation, added weather icons and description, deleted custom weather icon source images

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ function formatDate(date) {
 }
 
 function displayCurrentTemperature(response) {
+  let h1 = document.querySelector("h1");
+   h1.innerHTML = `${response.data.name}`;
+  let temperature = Math.round(response.data.main.temp);
   document.querySelector("#today-temp").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -34,6 +37,8 @@ function displayCurrentTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#current-description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function searchCity(event) {
@@ -57,15 +62,16 @@ let temp = Number(document.querySelector("#today-temp").innerHTML);
 const fahrenheitResult = Math.round(temp*9/5) + 32;
 // console.log("fahrenheit",fahrenheitResult);
 document.querySelector("#today-temp").innerHTML = fahrenheitResult;
+  fahrenheitButton.removeEventListener("click", convertToF);
+  celsiusButton.addEventListener("click", convertToC);
 }
-
 function convertToC () {
   let temp= Number(document.querySelector("#today-temp").innerHTML);
-  const celsiusResult = Math.round(temp-32)*5/9;
+  const celsiusResult = Math.round(((temp-32)*5)/9);
   document.querySelector("#today-temp").innerHTML = celsiusResult;
+  fahrenheitButton.addEventListener("click", convertToF);
+  celsiusButton.removeEventListener("click", convertToC);
 }
-
-//Need to prevent buttons from calculating too much
 
 let searchCityForm = document.querySelector("#search-city-form");
 searchCityForm.addEventListener("submit", searchCity);
@@ -77,19 +83,17 @@ let celsiusButton = document.querySelector("#celsius-link");
 celsiusButton.addEventListener("click", convertToC);
 
 //Geolocation
+function retrievePosition(position) {
+  let apiKey = "5888f249350e3151a80f06ae9f2e6ba9";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(displayCurrentTemperature);
+}
 
+navigator.geolocation.getCurrentPosition(retrievePosition);
 
+ //Date and Time
 let dateElement = document.querySelector("#todays-date-and-time");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
-//How to get the weather icons for openweathermap api
-//https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
-//var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-//data.data.weather[0].icon
-//data.data.weather[0].description 
-
-//create a  function and name it display weather info
-//var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-//query select ID of weather icon and update the SRC to be weather icon url (refer to line 83 with the article)
-//query select ID of weather description and update innerHTML text to data.data.weather[0].description 
